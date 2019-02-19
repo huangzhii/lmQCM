1e1a561cbb72635183c82154878d55eb07179112
Zhi Huang <huangzz@umich.edu>
2019-02-19 14:34:17 -0500

    update 2.0, add various functions

---
+++ R/fastFilter.R
@@ -0,0 +1,74 @@
+#' fastFilter: Subroutine for filtering expression matrix
+#'
+#' Author: Zhi Huang
+#' @param RNA an expression matrix (rows: genes; columns: samples)
+#' @param lowest_percentile_mean a float value range 0-1
+#' @param lowest_percentile_variance a float value range 0-1
+#' @return An filtered expression matrix
+#' @import genefilter
+#' @import Biobase
+#' @import nnet
+#' @import stats
+#' @export
+fastFilter <- function (RNA, lowest_percentile_mean = 0.2, lowest_percentile_variance = 0.2){
+  rowIQRs <- function(eSet) {
+    numSamp <- ncol(eSet)
+    lowQ <- rowQ(eSet, floor(0.25 * numSamp))
+    upQ <- rowQ(eSet, ceiling(0.75 * numSamp))
+    upQ - lowQ
+  }
+  varFilter <- function (eset, var.func = IQR, var.cutoff = 0.5, filterByQuantile = TRUE) {
+    if (deparse(substitute(var.func)) == "IQR") {
+      message("Using row-wise IQR for calculating the variances.")
+      vars <- rowIQRs(eset)
+    } else {
+      vars <- apply(exprs(eset), 1, var.func)
+    }
+    if (filterByQuantile) {
+      if (0 < var.cutoff && var.cutoff < 1) {
+        quant = quantile(vars, probs = var.cutoff)
+        selected = !is.na(vars) & vars > quant
+      } else stop("Cutoff Quantile has to be between 0 and 1.")
+    } else {
+      selected <- !is.na(vars) & vars > var.cutoff
+    }
+    return(selected)
+  }
+
+  # Remove data with lowest m% mean exp value shared by all samples
+  message("Note: For RNA data, we suppose input matrix (data frame) is with:")
+  message("      Row: Genes;    Columns: Samples.")
+  geneID = rownames(RNA)
+  percentile = lowest_percentile_mean
+  if (lowest_percentile_mean > 0){
+    RNAmean = apply(RNA, 1, mean)
+    RNA_filtered1 = RNA[RNAmean > quantile(RNAmean, percentile), ]
+    geneID_filtered1 = geneID[RNAmean > quantile(RNAmean, percentile)]
+  } else {
+    RNA_filtered1 = RNA
+    geneID_filtered1 = geneID
+  }
+  message(sprintf("(%d genes, %d samples) after removing lowest %.2f%% mean expression value.",
+                  dim(RNA_filtered1)[1], dim(RNA_filtered1)[2], lowest_percentile_mean*100))
+
+  # Remove data with lowest 10% variance across samples
+  percentile = lowest_percentile_variance
+  if (percentile > 0){
+    if (dim(RNA_filtered1)[2] > 3){
+      index <- varFilter(eset = RNA_filtered1, var.cutoff = percentile)
+      RNA_filtered2 = RNA_filtered1[index, ]
+      geneID_filtered2 = geneID_filtered1[index]
+    } else{
+      message("Cannot calculate order statistic on object with less than 3 columns, will not remove data based on variance.")
+      RNA_filtered2 = RNA_filtered1
+      geneID_filtered2 = geneID_filtered1
+    }
+  } else {
+    RNA_filtered2 = RNA_filtered1
+    geneID_filtered2 = geneID_filtered1
+  }
+
+  message(sprintf("(%d genes, %d samples) after removing lowest %.2f%% variance expression value.",
+                  dim(RNA_filtered2)[1], dim(RNA_filtered2)[2], lowest_percentile_mean*100))
+  return(RNA_filtered2)
+}
--- R/lmQCM.R
+++ R/lmQCM.R
@@ -1,3 +1,5 @@
+setClass("QCMObject", representation(clusters.id = "list", clusters.names = "list",
+                                     eigengene.matrix = "data.frame"))
 #' lmQCM: Main Routine for Gene Co-expression Analysis
 #'
 #' Author: Zhi Huang
@@ -9,13 +11,14 @@
 #' @param minClusterSize minimum length of cluster to retain (default = 10)
 #' @param CCmethod Methods for correlation coefficient calculation (default = "pearson"). Users can also pick "spearman".
 #' @param normalization Determine if normalization is needed on massive correlation coefficient matrix.
-#' @return mergedCluster - An merged clusters group
+#' @return QCMObject - An S4 Class with lmQCM results
 #'
 #' @examples
 #' library(lmQCM)
 #' library(Biobase)
 #' data(sample.ExpressionSet)
 #' data = assayData(sample.ExpressionSet)$exprs
+#' data = fastFilter(data, 0.2, 0.2)
 #' lmQCM(data)
 #'
 #' @import genefilter
@@ -23,7 +26,6 @@
 #' @import nnet
 #' @import stats
 #' @export
-
 lmQCM <- function(data_in,gamma=0.55,t=1,lambda=1,beta=0.4,minClusterSize=10,CCmethod="pearson", normalization = F) {
   message("Calculating massive correlation coefficient ...")
   cMatrix <- cor(t(data_in), method = CCmethod)
@@ -40,7 +42,33 @@ lmQCM <- function(data_in,gamma=0.55,t=1,lambda=1,beta=0.4,minClusterSize=10,CCm
   }
 
   C <- localMaximumQCM(cMatrix, gamma, t, lambda)
-  mergedCluster <- merging_lmQCM(C, beta, minClusterSize)
+
+  clusters <- merging_lmQCM(C, beta, minClusterSize)
+  # map rownames to clusters
+  clusters.names = list()
+  for (i in 1:length(clusters)){
+    mc = clusters[[i]]
+    clusters.names[[i]] = rownames(data)[mc]
+  }
+  # calculate eigengene
+  eigengene.matrix <- matrix(0, nrow = length(clusters), ncol = dim(data)[2]) # Clusters * Samples
+
+  for (i in 1:(length(clusters.names))) {
+    geneID <- as.matrix(clusters.names[[i]])
+    X <- data[geneID,]
+    mu <- rowMeans(X)
+    stddev <- rowSds(as.matrix(X), na.rm=TRUE) # standard deviation with 1/(n-1)
+    XNorm <- sweep(X,1,mu) # normalize X
+    XNorm <- apply(XNorm, 2, function(x) x/stddev)
+    SVD <- svd(XNorm, LINPACK = FALSE)
+    eigengene.matrix[i,] <- t(SVD$v[,1])
+  }
+  eigengene.matrix = data.frame(eigengene.matrix)
+  colnames(eigengene.matrix) = colnames(data)
+
+  QCMObject <- new("QCMObject", clusters.id = clusters, clusters.names = clusters.names,
+                   eigengene.matrix = eigengene.matrix)
+
   message("Done.")
-  return(mergedCluster)
+  return(QCMObject)
 }
