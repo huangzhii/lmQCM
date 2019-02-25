57040a9eb8b01a0579c9966fd937e93021a57d54
Zhi Huang <huangzz@umich.edu>
2019-02-25 18:33:46 -0500

    version 0.2.1

--- R/fastFilter.R
+++ R/fastFilter.R
@@ -4,25 +4,28 @@
 #' @param RNA an expression matrix (rows: genes; columns: samples)
 #' @param lowest_percentile_mean a float value range 0-1
 #' @param lowest_percentile_variance a float value range 0-1
+#' @param var.func specify variance function
 #' @return An filtered expression matrix
 #' @import genefilter
 #' @import Biobase
 #' @import nnet
 #' @import stats
 #' @export
-fastFilter <- function (RNA, lowest_percentile_mean = 0.2, lowest_percentile_variance = 0.2){
+fastFilter <- function (RNA, lowest_percentile_mean = 0.2, lowest_percentile_variance = 0.2, var.func = "var"){
+  RNA = as.matrix(RNA)
   rowIQRs <- function(eSet) {
     numSamp <- ncol(eSet)
     lowQ <- rowQ(eSet, floor(0.25 * numSamp))
     upQ <- rowQ(eSet, ceiling(0.75 * numSamp))
     upQ - lowQ
   }
-  varFilter <- function (eset, var.func = IQR, var.cutoff = 0.5, filterByQuantile = TRUE) {
+  varFilter <- function (eset, var.cutoff = 0.5, filterByQuantile = TRUE, var.func = var.func) {
     if (deparse(substitute(var.func)) == "IQR") {
       message("Using row-wise IQR for calculating the variances.")
       vars <- rowIQRs(eset)
     } else {
-      vars <- apply(exprs(eset), 1, var.func)
+      message("Calculating the variances.")
+      vars <- apply(eset, 1, var.func)
     }
     if (filterByQuantile) {
       if (0 < var.cutoff && var.cutoff < 1) {
@@ -40,7 +43,7 @@ fastFilter <- function (RNA, lowest_percentile_mean = 0.2, lowest_percentile_var
   message("      Row: Genes;    Columns: Samples.")
   geneID = rownames(RNA)
   percentile = lowest_percentile_mean
-  if (lowest_percentile_mean > 0){
+  if (percentile > 0){
     RNAmean = apply(RNA, 1, mean)
     RNA_filtered1 = RNA[RNAmean > quantile(RNAmean, percentile), ]
     geneID_filtered1 = geneID[RNAmean > quantile(RNAmean, percentile)]
@@ -49,13 +52,13 @@ fastFilter <- function (RNA, lowest_percentile_mean = 0.2, lowest_percentile_var
     geneID_filtered1 = geneID
   }
   message(sprintf("(%d genes, %d samples) after removing lowest %.2f%% mean expression value.",
-                  dim(RNA_filtered1)[1], dim(RNA_filtered1)[2], lowest_percentile_mean*100))
+                  dim(RNA_filtered1)[1], dim(RNA_filtered1)[2], percentile*100))
 
   # Remove data with lowest 10% variance across samples
   percentile = lowest_percentile_variance
   if (percentile > 0){
     if (dim(RNA_filtered1)[2] > 3){
-      index <- varFilter(eset = RNA_filtered1, var.cutoff = percentile)
+      index <- varFilter(eset = RNA_filtered1, var.cutoff = percentile, var.func = var.func)
       RNA_filtered2 = RNA_filtered1[index, ]
       geneID_filtered2 = geneID_filtered1[index]
     } else{
